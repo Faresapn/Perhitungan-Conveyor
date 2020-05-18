3faac6f9fa82f8e5cac6951fea6cc29e72995856
Qoolqas <qoolqas@gmail.com>
2020-05-18 19:19:31 +0700

    getString extra

--- app/src/main/java/com/faresa/perhitungankonveyor/ui/screw/HasilScrewActivity.java
+++ app/src/main/java/com/faresa/perhitungankonveyor/ui/screw/HasilScrewActivity.java
@@ -7,10 +7,24 @@ import android.os.Bundle;
 import com.faresa.perhitungankonveyor.R;
 
 public class HasilScrewActivity extends AppCompatActivity {
+    String hpf, hpm, hp, hp2, torque, torque2, angle, hph, incHp, incHp2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hasil_screw);
+
+        hpf = getIntent().getStringExtra("hpf");
+        hpm = getIntent().getStringExtra("hpm");
+        hp = getIntent().getStringExtra("hp");
+        hp2 = getIntent().getStringExtra("hp2");
+        torque = getIntent().getStringExtra("torque");
+        torque2 = getIntent().getStringExtra("torque2");
+        angle = getIntent().getStringExtra("angle");
+        hph = getIntent().getStringExtra("hph");
+        incHp = getIntent().getStringExtra("inc_hp");
+        incHp2 = getIntent().getStringExtra("inc_hp2");
+
+
     }
 }
